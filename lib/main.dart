e0faa654b537b42d85f12833fe0ffa39df5c3207
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-06-07 19:24:25 +0300

    Открытие store и создание репозитория

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:maps_example/main_screen.dart';
+import 'package:maps_example/model/route_entity.dart';
 import 'package:maps_example/screens/yandex_map_page.dart';
-
+import 'main_screen.dart';
+import 'model/repository.dart';
+import 'objectbox.g.dart';
 import 'screens/google_map_page.dart';
 
 const googleApiKey = "AIzaSyBLR3iEOULZSNtuNNhhGLIpTASvwxvVLg4";
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final Store store = await openStore();
+  final Box<RouteEntity> box = Box<RouteEntity>(store);
+  final repositary = RoutesRepository(box);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  MainScreen(routesRepository:repositary),
+
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        // home: const YandexMapPage());
-        //home: const YandexMapPageRoutes());
-        //home: const GoogleMapPage());
-        home: MainScreen());
-  }
-}
